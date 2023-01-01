05ec6f00698fa36bc58f599e36b609c4d3d86704
unknown <zaryabzak22@gmail.com>
2023-01-01 20:57:49 +0500

    Error handling for unsupported routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 //Own imports
 const placesRoutes = require("./routes/places-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
@@ -11,6 +12,12 @@ app.use(bodyParser.json());
 
 //Registering placesRoutes as a middleware with a filter /api/places so that all the routes with this specific middleware should start with /api/places...
 app.use("/api/places", placesRoutes);
+
+//This middleware is registered for requests that don't send a response back or unsupported routes. We are throwing an error in this middleware so the error will be caught in the special middleware with the error parameter
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
 
 //Express will apply this middleware on every request. If you provide 3 parameters to any middle ware, Express will understand it as a special middleware for error handling
 app.use((error, req, res, next) => {
