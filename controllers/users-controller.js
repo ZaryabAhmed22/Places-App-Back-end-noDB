504c9ef1bb02840057454e12fb3d70a68e9c46d7
unknown <zaryabzak22@gmail.com>
2023-01-12 12:24:08 +0500

    Finished working with users routes

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -8,16 +8,14 @@ const DUMMY_USERS = [
   {
     id: "u1",
     name: "Zaryab",
-    image:
-      "https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
-    places: 3,
+    email: "test@test.com",
+    password: "testsss",
   },
   {
     id: "u2",
     name: "Jonas",
-    image:
-      "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
-    places: 5,
+    email: "jonas@test.com",
+    password: "jonastest",
   },
 ];
 
@@ -30,6 +28,15 @@ const getUserById = (req, res, next) => {
 const createUser = (req, res, next) => {
   //This is the data sent by signup form
   const { name, email, password } = req.body;
+
+  //Checking if the user already exists with the same provided email
+  const hasUser = DUMMY_USERS.find((user) => {
+    user.email === email;
+  });
+
+  if (hasUser) {
+    throw new HttpError("User already exists", 422);
+  }
 
   //Creating a new User
   const createdUser = {
@@ -52,10 +59,15 @@ const loginUser = (req, res, next) => {
 
   //Returning the matched user
   const user = DUMMY_USERS.find((user) => {
-    return (
-      name === user.name && email === user.email && password === user.password
-    );
+    return email === user.email && password === user.password;
   });
+
+  //Checking id the user with provided credentials exists
+  if (!user) {
+    throw new HttpError("Wrong credentials, could not find user", 401);
+  }
+
+  res.json({ message: "logged in" });
 };
 //Exporting are functions >> this is the syntax used to export multiple functions
 exports.getUserById = getUserById;
